c18633b3867c5a7bc1eca511ca5c178df6e93b2d
이정수 <jslblar080@gmail.com>
2025-08-29 23:44:14 +0000

    test: generalize simplified self-attention test for all tokens

--- tests/learning_tests/test_attention_learning.py
+++ tests/learning_tests/test_attention_learning.py
@@ -6,25 +6,25 @@ class TestAttentionLearning:
     def test_simplified_self_attention(self):
         inputs = torch.tensor(
             [
-                [0.43, 0.15, 0.89],  # x_1
-                [0.55, 0.87, 0.66],  # x_2 (query)
-                [0.57, 0.85, 0.64],  # x_3
-                [0.22, 0.58, 0.33],  # x_4
-                [0.77, 0.25, 0.10],  # x_5
-                [0.05, 0.80, 0.55],  # x_6
+                [0.43, 0.15, 0.89],  # x_0
+                [0.55, 0.87, 0.66],  # x_1 (query)
+                [0.57, 0.85, 0.64],  # x_2
+                [0.22, 0.58, 0.33],  # x_3
+                [0.77, 0.25, 0.10],  # x_4
+                [0.05, 0.80, 0.55],  # x_5
             ]
         )
-        query = inputs[1]
-        attn_scores_query = torch.empty(inputs.shape[0])
+        attn_scores = torch.empty(inputs.shape[0], inputs.shape[0])
+        """
         for i, x_i in enumerate(inputs):
-            attn_scores_query[i] = torch.dot(x_i, query)
-        print("\nAttention scores:", attn_scores_query)
-        attn_weights_query = torch.softmax(attn_scores_query, dim=0)
-        print("Attention weights:", attn_weights_query)
-        attn_weights_query_sum = attn_weights_query.sum().item()
-        print("Sum of attention weights:", attn_weights_query_sum)
-        torch.testing.assert_close(attn_weights_query_sum, 1.0)
-        context_vec_query = torch.zeros(query.shape)
-        for i, x_i in enumerate(inputs):
-            context_vec_query += attn_weights_query[i] * x_i
-        print(context_vec_query)
+            for j, x_j in enumerate(inputs):
+                attn_scores[i, j] = torch.dot(x_i, x_j)
+        """
+        attn_scores = inputs @ inputs.T  # matrix multiplication
+        print("\nAttention scores:\n", attn_scores)
+        attn_weights = torch.softmax(attn_scores, dim=-1)
+        print("Attention weights:\n", attn_weights)
+        for sum in attn_weights.sum(dim=-1):
+            torch.testing.assert_close(sum.item(), 1.0)
+        all_context_vecs = attn_weights @ inputs
+        print("All context vectors:\n", all_context_vecs)
