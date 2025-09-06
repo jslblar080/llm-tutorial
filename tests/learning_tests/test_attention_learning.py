5983b84a8afbdf0bf0906bf8d7c33698f2ebe9a2
이정수 <jslblar080@gmail.com>
2025-09-06 09:10:16 +0000

    test: add multi-head attention class for single-head attention test

--- tests/learning_tests/test_attention_learning.py
+++ tests/learning_tests/test_attention_learning.py
@@ -89,8 +89,8 @@ class TestAttentionLearning:
         _W_query: nn.Linear
         _W_key: nn.Linear
         _W_value: nn.Linear
-        _dropout: nn.Dropout
         _mask: Tensor
+        _dropout: nn.Dropout
 
         def __init__(
             self,
@@ -106,11 +106,11 @@ class TestAttentionLearning:
             self._W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
             self._W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
             self._W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
-            self._dropout = nn.Dropout(dr)
             # buffers automatically move to appropriate device (CPU or GPU)
             self.register_buffer(
                 "_mask", torch.triu(torch.ones(cxt_len, cxt_len), diagonal=1)
             )
+            self._dropout = nn.Dropout(dr)
 
         def forward(self, x):
             batch_size, cxt_len, d_in = x.shape
@@ -124,6 +124,77 @@ class TestAttentionLearning:
             attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
             attn_weights = self._dropout(attn_weights)
             all_context_vecs = attn_weights @ values
+            return all_context_vecs
+
+    class MultiHeadAttention(nn.Module):
+
+        _n_head: int
+        _d_head: int
+        _W_query: nn.Linear
+        _W_key: nn.Linear
+        _W_value: nn.Linear
+        _mask: Tensor
+        _dropout: nn.Dropout
+        _out_proj: nn.Identity | nn.Linear
+
+        def __init__(
+            self,
+            d_in: int,
+            d_out: int,
+            cxt_len: int,
+            dr: float,
+            n_head: int,
+            seed_num: int,
+            qkv_bias=False,
+            out_proj=False,
+        ):
+            super().__init__()
+            torch.manual_seed(seed_num)
+            assert d_out % n_head == 0, "d_out must be divisible by n_head"
+            self._n_head = n_head
+            self._d_head = d_out // n_head
+            self._W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+            self._W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+            self._W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+            self.register_buffer(
+                "_mask", torch.triu(torch.ones(cxt_len, cxt_len), diagonal=1)
+            )
+            self._dropout = nn.Dropout(dr)
+            self._out_proj = nn.Linear(d_out, d_out) if out_proj else nn.Identity()
+
+        def forward(self, x):
+            batch_size, cxt_len, d_in = x.shape
+            # unlike reshape, view requires contiguous tensor
+            # output tensor of nn.Linear is always contiguous
+            queries = (
+                self._W_query(x)
+                .view(batch_size, cxt_len, self._n_head, self._d_head)
+                .transpose(1, 2)
+            )
+            keys = (
+                self._W_key(x)
+                .view(batch_size, cxt_len, self._n_head, self._d_head)
+                .transpose(1, 2)
+            )
+            values = (
+                self._W_value(x)
+                .view(batch_size, cxt_len, self._n_head, self._d_head)
+                .transpose(1, 2)
+            )
+            attn_scores: Tensor = queries @ keys.transpose(
+                2, 3  # 0: batch_size, 1: self._n_head, 2: cxt_len, 3: self._d_head
+            )
+            attn_scores.masked_fill_(self._mask.bool(), -torch.inf)
+            attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+            attn_weights = self._dropout(attn_weights)
+            all_context_vecs = (
+                (attn_weights @ values)
+                .transpose(1, 2)
+                .contiguous()
+                .view(batch_size, cxt_len, self._n_head * self._d_head)
+            )
+            # optional linear projection
+            all_context_vecs = self._out_proj(all_context_vecs)
             return all_context_vecs
 
     _arr = [
@@ -226,9 +297,20 @@ class TestAttentionLearning:
         d_in = self._batch.shape[2]
         d_out = self._batch.shape[2]
         cxt_len = self._batch.shape[1]
-        ca = self.CausalAttention(d_in, d_out, cxt_len, 0.0, self._seed_num)
+        dr = 0.0
+        ca = self.CausalAttention(d_in, d_out, cxt_len, dr, self._seed_num)
         all_context_vecs = ca(self._batch)
         print("\nall_context_vecs.shape:", all_context_vecs.shape)
         sal = self.SelfAttentionLinear(d_in, d_out, self._seed_num)
         # no masking on last row with dropout rate 0.0
         torch.testing.assert_close(all_context_vecs[0][-1], sal(self._inputs)[-1])
+
+    def test_single_head_attention(self):
+        d_in = self._batch.shape[2]
+        d_out = self._batch.shape[2]
+        cxt_len = self._batch.shape[1]
+        dr = 0.0
+        n_head = 1
+        sha = self.MultiHeadAttention(d_in, d_out, cxt_len, dr, n_head, self._seed_num)
+        ca = self.CausalAttention(d_in, d_out, cxt_len, dr, self._seed_num)
+        torch.testing.assert_close(sha(self._batch), ca(self._batch))
