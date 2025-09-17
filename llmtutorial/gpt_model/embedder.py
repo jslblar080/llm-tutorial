8a1e16ed1202c548ddf2b5017812ce01714b79cd
이정수 <jslblar080@gmail.com>
2025-09-17 15:08:52 +0000

    chore: add option to print weight matrix of token embedding layer

--- llmtutorial/gpt_model/embedder.py
+++ llmtutorial/gpt_model/embedder.py
@@ -8,14 +8,16 @@ from .gpt_model_config import GPTModelConfig
 class Embedder:
 
     @staticmethod
-    def _token_layer() -> nn.Embedding:
+    def _token_layer(verbose: bool) -> nn.Embedding:
 
         token_embedding_layer = nn.Embedding(
             GPTModelConfig().num_embeddings, GPTModelConfig().embedding_dim
         )
-        # print(
-        #     "\nWeight matrix of token embedding layer:\n", token_embedding_layer.weight
-        # )
+        if verbose:
+            print(
+                "\nWeight matrix of token embedding layer:\n",
+                token_embedding_layer.weight,
+            )
 
         return token_embedding_layer
 
@@ -27,11 +29,11 @@ class Embedder:
         return pos_embedding_layer
 
     @classmethod
-    def input_embeddings(cls, inputs: Tensor) -> Tensor:
+    def input_embeddings(cls, inputs: Tensor, verbose=False) -> Tensor:
 
         batch_size, cxt_len = inputs.shape
 
-        token_embedding_layer = cls._token_layer()
+        token_embedding_layer = cls._token_layer(verbose)
         token_embeddings = token_embedding_layer(inputs)
 
         pos_embedding_layer = cls._pos_layer(cxt_len)
